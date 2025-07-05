b8d25688e64fb48b517db28bf8ebf145693be432
parth-18-bansal <parthbansalcoding@gmail.com>
2025-07-06 00:38:40 +0530

    created the state entity

---
+++ care/src/main/java/com/care/care/Entities/StateEntity.java
@@ -0,0 +1,24 @@
+package com.care.care.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+}

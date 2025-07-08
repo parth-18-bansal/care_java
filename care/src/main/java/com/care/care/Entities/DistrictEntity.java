832d710ccd0da84fe98f2f19e2e3f89c6ab3a174
parth-18-bansal <parthbansalcoding@gmail.com>
2025-07-09 00:55:54 +0530

    districts entity

--- care/src/main/java/com/care/care/Entities/DistrictEntity.java
+++ care/src/main/java/com/care/care/Entities/DistrictEntity.java
@@ -5,22 +5,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "states")
 @Data
+@Table(name = "districts")
 @AllArgsConstructor
 @NoArgsConstructor
-public class StateEntity {
+public class DistrictEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "state_id", nullable = false)
+    @org.hibernate.annotations.OnDelete(action = org.hibernate.annotations.OnDeleteAction.NO_ACTION)
+    private StateEntity state;
+
     @Column(nullable = false, length = 255)
     private String name;
+
 }
